e9605736d5f24578a285c4a3ffc4b3412f2b18b1
S.G <19246983+m1nicrusher@users.noreply.github.com>
2023-05-27 23:42:25 +1000

    Provide PortalColor enum ability to convert to string name

--- pacman/src/models/entities/Portal.java
+++ pacman/src/models/entities/Portal.java
@@ -3,29 +3,35 @@ package src.models.entities;
 import src.game.PortalManager;
 import src.models.Collidable;
 import src.models.Entity;
+import src.models.GameVersion;
 
 public class Portal extends Entity implements Collidable
 {
-	/** Enum for colors of portals */
-	public enum PortColor {
-		White, Yellow, DarkGold, DarkGray
-	}
-	public PortColor color;
-
 	// Sprite images path
 	static final String WHITE_PORTAL = "sprites/portalWhite.png";
 	static final String YELLOW_PORTAL = "sprites/portalYellow.png";
 	static final String GOLD_PORTAL = "sprites/portalDarkGold.png";
 	static final String GRAY_PORTAL = "sprites/portalDarkGray.png";
 
-	public Portal(PortColor c)
+	private PortalColor _color;
+	private PortalManager _manager;
+
+	public Portal(PortalColor c)
 	{
 		super(getImgPath(c));
-		this.color = c;
+		this._color = c;
 	}
 
+	public PortalColor getColor() { return _color; }
+
+	public void setColor(PortalColor color) { _color = color; }
+
+	public PortalManager getManager() { return _manager; }
+
+	public void setManager(PortalManager manager) { _manager = manager; }
+
 	/** Function that finds correct image for initialise portal entity, return null if not found */
-	private static String getImgPath(PortColor c)
+	private static String getImgPath(PortalColor c)
 	{
 		switch (c)
 		{
@@ -42,10 +48,30 @@ public class Portal extends Entity implements Collidable
 	{
 		if (collidable instanceof Entity)
 		{
-			PortalManager.teleport((Entity) collidable, this);
+			getManager().teleport((Entity) collidable, this);
 		}
 	}
 
 	@Override
-	public Object clone() { return new Portal(color); }
+	public Object clone() { return new Portal(_color); }
+
+	/** Enum for colors of portals */
+	public enum PortalColor
+	{
+		White("White"),
+		Yellow("Yellow"),
+		DarkGold("DarkGold"),
+		DarkGray("DarkGray");gi
+		private final String _value;
+
+		PortalColor(String name)
+		{
+			_value = name;
+		}
+
+		public String getColorName()
+		{
+			return _value;
+		}
+	}
 }
